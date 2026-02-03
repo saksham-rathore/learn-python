4eb0aed0ebf5a54b5b218d5491326f0905c14a75
saksham762007 <samrathore07062007@gmail.com>
2026-02-03 22:33:23 +0530

    file update

--- problem_solving.py/problem.py
+++ problem_solving.py/problem.py
@@ -1,5 +1,9 @@
 my_list = [10, 5, 20, 15, 25]
 print("Initial List:", my_list)
-del my_list[0]
-print("\nAfter Deleting element at index 0:")
+# my_list.sort()
+# print("\nList after Sorting (Ascending):")
+# print(my_list)
+
+my_list.sort(reverse=True)
+print("\nList after Sorting (Descending):")
 print(my_list)
